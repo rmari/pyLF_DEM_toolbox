e053c590ec08edb726b3d8b098b32a151c349d63
Romain Mari <romain.mari@gmail.com>
2017-05-01 15:59:58 +0100

    alternate echo/shear with arbitrary interactions

--- alternate_echo_shear.py
+++ alternate_echo_shear.py
@@ -17,6 +17,9 @@ def getArgParser():
     parser.add_argument('-a', '--amplitude', type=float, required=True)
     parser.add_argument('-g', '--shear_strain', type=float, required=True)
     parser.add_argument('-p', '--oscillation_nb', type=int, required=True)
+    parser.add_argument('-r', '--rate_primary',
+                        type=str, required=False, default="1h")
+    parser.add_argument('-t', '--time_ratio', type=float)
 
     return parser
 
@@ -60,6 +63,9 @@ def getSimuName(in_args, control_var):
                  + "_amplitude"+str(in_args['amplitude'])\
                  + "_"+str(in_args['oscillation_nb'])+"periods"\
                  + "_"+str(in_args['shear_strain'])+"straight"
+    if in_args['time_ratio'] is not None:
+        simu_name += "_rate"+str(in_args['rate_primary'])
+
     return simu_name
 
 
@@ -96,14 +102,27 @@ def setupSimulation(in_args, simu):
 
     simu.force_to_run = in_args['overwrite']
 
-    simu.setControlVariable("rate")
+    if in_args['time_ratio'] is not None\
+            and in_args['rate_primary'] == "1h":
+        print(" time ratio useless in rate independent rheology.",
+              file=sys.stderr)
+        sys.exit(1)
+    if in_args['time_ratio'] is None\
+            and in_args['rate_primary'] != "1h":
+        print("needs time ratio in rate dependent rheology.",
+              file=sys.stderr)
+        sys.exit(1)
 
-    simu.setDefaultParameters("h")
+    primary_rate, unit = getSuffix(in_args['rate_primary'])
+    control_var = "rate"
+    simu.setControlVariable(control_var)
+
+    simu.setDefaultParameters(unit)
     simu.readParameterFile(in_args['params_file'])
     simu.p.cross_shear = True
     simu.tagStrainParameters()
 
-    simu.setupNonDimensionalization(1, "h")
+    simu.setupNonDimensionalization(primary_rate, unit)
 
     simu.assertParameterCompatibility()
     simu.resolveTimeOrStrainParameters()
@@ -113,7 +132,7 @@ def setupSimulation(in_args, simu):
     system = simu.getSys()
 
     system.setupSystemPostConfiguration()
-    simu.simu_name = getSimuName(in_args, "rate")
+    simu.simu_name = getSimuName(in_args, control_var)
     simu.openOutputFiles(simu.simu_name)
     echoInput(in_args, simu)
 
@@ -160,19 +179,30 @@ def alternate_simu(in_args):
     tk.addClock("config",
                 lfdem.LinearClock(simu.p.time_interval_output_config, True))
 
-    echoing_time = 4*in_args["oscillation_nb"]*in_args["amplitude"]
-    alternating_period = echoing_time + in_args["shear_strain"]
+    n = in_args["oscillation_nb"]
+    gamma_OSP = in_args["amplitude"]
+    gamma_para = in_args["shear_strain"]
+    echoing_strain = 4*n*gamma_OSP
+    alternating_period = echoing_strain + gamma_para
     tk.addClock("echo_on",
                 lfdem.LinearClock(alternating_period,
                                   True))
     tk.addClock("shear_on",
-                lfdem.LinearClock(echoing_time,
+                lfdem.LinearClock(echoing_strain,
                                   alternating_period,
                                   True))
     tk.addClock("reverse",
-                lfdem.LinearClock(in_args["amplitude"],
-                                  2*in_args["amplitude"],
+                lfdem.LinearClock(gamma_OSP,
+                                  2*gamma_OSP,
                                   True))
+    if in_args["time_ratio"] is not None:
+        echo_ratio = in_args["time_ratio"]
+        avg_rate = system.get_shear_rate()
+        para_rate = avg_rate/(1-echo_ratio)
+        echoing_rate = 4*n*gamma_OSP*avg_rate/gamma_para/echo_ratio
+    else:
+        para_rate = system.get_shear_rate()
+        echoing_rate = system.get_shear_rate()
     binconf_counter = 0
     shear_dir = np.pi/2
     system.setShearDirection(shear_dir)
@@ -188,12 +218,15 @@ def alternate_simu(in_args):
 
         if "shear_on" in events:
             tk.removeClock("reverse")
+            system.set_shear_rate(para_rate)
         if "echo_on" in events:
+            current_strain = system.get_curvilinear_strain()
             reverse_clock =\
-                lfdem.LinearClock(system.get_time()+in_args["amplitude"],
-                                  2*in_args["amplitude"],
+                lfdem.LinearClock(current_strain+gamma_OSP,
+                                  2*gamma_OSP,
                                   True)
             tk.addClock("reverse", reverse_clock)
+            system.set_shear_rate(echoing_rate)
         simu.printProgress()
 
     print("Time evolution done")
